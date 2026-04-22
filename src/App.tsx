6bbf06794be7528541d00b476e0e22f03555f832
Zofia Drozd <drozd.zosia@gmail.com>
2026-04-22 10:44:58 +0200

    refactor: add length const

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import { createNoise2D } from "simplex-noise";
 
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 600;
@@ -28,14 +29,15 @@ function App() {
 			canvasCtx.strokeStyle = "purple";
 
 			const STEP = 20;
+			const SEGMENT_LENGTH = 10;
 			for (let i = STEP; i < CANVAS_WIDTH; i += STEP) {
 				for (let j = STEP; j < CANVAS_HEIGHT; j += STEP) {
 					canvasCtx.beginPath();
 					canvasCtx.moveTo(i, j);
 					const angle = (i / j + offset) * Math.PI * 2;
 					canvasCtx.lineTo(
-						i + Math.cos(angle) * 10,
-						j + Math.sin(angle) * 10,
+						i + Math.cos(angle) * SEGMENT_LENGTH,
+						j + Math.sin(angle) * SEGMENT_LENGTH,
 					);
 					canvasCtx.stroke();
 				}
