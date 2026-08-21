b6dc177838eb6ca5b9e399259d0d14ad1f863646
Zofia Drozd <drozd.zosia@gmail.com>
2026-08-21 23:40:01 +0200

    feat: color all pixels based on distance

--- src/sketches/Voronoi/sketch.ts
+++ src/sketches/Voronoi/sketch.ts
@@ -7,18 +7,19 @@ type Params = {
 	CANVAS_WIDTH: number;
 };
 
-export default ({
-	canvasCtx,
-	paramsRef,
-	CANVAS_HEIGHT,
-	CANVAS_WIDTH,
-}: Params) => {
+const MAX_RGB = 256;
+
+export default ({ canvasCtx, CANVAS_HEIGHT, CANVAS_WIDTH }: Params) => {
 	let drawRequestId = 0;
 
 	const particles = Array.from({ length: 20 }, () => ({
 		x: Math.random() * CANVAS_WIDTH,
 		y: Math.random() * CANVAS_HEIGHT,
-		paramsRef,
+		color: {
+			r: Math.floor(Math.random() * MAX_RGB),
+			g: Math.floor(Math.random() * MAX_RGB),
+			b: Math.floor(Math.random() * MAX_RGB),
+		},
 	}));
 
 	if (!canvasCtx) {
@@ -27,8 +28,34 @@ export default ({
 
 	const draw = () => {
 		drawRequestId = requestAnimationFrame(draw);
+		const imageData = canvasCtx.createImageData(
+			CANVAS_WIDTH,
+			CANVAS_HEIGHT,
+		);
+		const data = imageData.data;
+
+		for (let j = 0; j < CANVAS_HEIGHT; j++) {
+			for (let i = 0; i < CANVAS_WIDTH; i++) {
+				let minDistance: number = Infinity;
+				let closestIdx: number = 0;
+				particles.forEach(({ x, y }, idx) => {
+					if (minDistance > (i - x) ** 2 + (j - y) ** 2) {
+						minDistance = (i - x) ** 2 + (j - y) ** 2;
+						closestIdx = idx;
+					}
+				});
+
+				const bufferStart = (j * CANVAS_WIDTH + i) * 4;
+				data[bufferStart] = particles[closestIdx].color.r; // R
+				data[bufferStart + 1] = particles[closestIdx].color.g; // G
+				data[bufferStart + 2] = particles[closestIdx].color.b; // B
+				data[bufferStart + 3] = 255; // A
+			}
+		}
+		canvasCtx.putImageData(imageData, 0, 0);
 
 		particles.forEach(({ x, y }) => {
+			canvasCtx.fillStyle = "black";
 			canvasCtx.fillRect(x, y, 3, 3);
 		});
 	};
